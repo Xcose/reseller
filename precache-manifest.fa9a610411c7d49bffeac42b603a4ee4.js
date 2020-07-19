b6786f4df9fe80dad3a4d05d3103f2b8bd2f55fa
Xcose <mfecanexhanti@gmail.com>
2020-07-20 01:27:38 +0200

    Updates

---
+++ precache-manifest.fa9a610411c7d49bffeac42b603a4ee4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2303f8189285ffe1a81d3841e677bf2",
+    "url": "/reseller/index.html"
+  },
+  {
+    "revision": "53dee9705a99b38549e0",
+    "url": "/reseller/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "514ed6957838ed6a45fc",
+    "url": "/reseller/static/css/main.40d0998b.chunk.css"
+  },
+  {
+    "revision": "53dee9705a99b38549e0",
+    "url": "/reseller/static/js/2.03d963be.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/reseller/static/js/2.03d963be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514ed6957838ed6a45fc",
+    "url": "/reseller/static/js/main.f6eb1d47.chunk.js"
+  },
+  {
+    "revision": "00f20eb5c56ba9e58572",
+    "url": "/reseller/static/js/runtime-main.68945eaa.js"
+  },
+  {
+    "revision": "be16282cbe9cb5ab0d16481619b1f5be",
+    "url": "/reseller/static/media/colour.be16282c.jpg"
+  },
+  {
+    "revision": "622d2c84cb90dc34d49fbb1f2d44d076",
+    "url": "/reseller/static/media/digitization.622d2c84.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reseller/static/media/logo.5d5d9eef.svg"
+  }
+]);
